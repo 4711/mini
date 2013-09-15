d71264c37a6fa06c710337bb4d00e0badf62ba7b
4711 <bzthcyqullt@mailinator.com>
2013-09-15 17:31:47 +0200

    initial commit

---
+++ js/app.js
@@ -0,0 +1,13 @@
+function MainController($scope) {
+  $scope.frameworks = [
+    { name: 'AngularJS',   url: 'angularjs.org' },
+    { name: 'Ember.js',    url: 'emberjs.com' },
+    { name: 'Backbone.js', url: 'backbonejs.org' }
+  ];
+  $scope.name = "Master";
+
+  $scope.addFramework = function() {
+    $scope.frameworks.push({ name:$scope.newFramework, url: 'www.' + $scope.newFramework + '.com'});
+    $scope.newFramework = '';
+  }
+}
